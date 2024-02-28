cdac2f95b89da8f9cf145a48438a58d7e5ba7afb
GabrielPeloo <gabriel.luiz.santos@hotmail.com>
2024-02-28 20:38:07 -0300

    perfil

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,17 +1,24 @@
-import { InstagramLogo, LinkedinLogo, GithubLogo } from '@phosphor-icons/react'
+import { GithubLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 function Footer() {
- 
-  
 
-  return (
-    <>
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  let footerComponent
+
+  let data = new Date().getFullYear()
+
+  if(usuario.token !== '') {
+    footerComponent = (
+      <>
         <div className="flex justify-center bg-indigo-900 text-white">
           <div className="container flex flex-col items-center py-4">
-            <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: </p>
+            <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: {data}</p>
             <p className='text-lg'>Acesse nossas redes sociais</p>
             <div className='flex gap-2'>
-                <a href="https://github.com/GabrielPeloo" target="_blank" className='hover:scale-90'>
+               <a href="https://github.com/GabrielPeloo" target="_blank" className='hover:scale-90'>
                     <GithubLogo size={60}/>
                 </a>
                 <a  href='https://www.linkedin.com/in/gabriel-luiz-pelo/' target="_blank" className='hover:scale-90'>
@@ -24,7 +31,25 @@ function Footer() {
           </div>
         </div>
       </>
+    )
+  }
+
+  return (
+    <>
+      {footerComponent}
+    </>
   )
 }
 
 export default Footer
+
+
+
+
+
+
+
+
+
+
+
